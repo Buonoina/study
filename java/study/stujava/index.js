0f9897d7b2940593de6de71aa648ace686e56d74
Buonoina <linghuadayo@gmail.com>
2023-01-26 12:39:30 +0900

    イベント発火　基礎編　修正

--- java/study/stujava/index.js
+++ java/study/stujava/index.js
@@ -1,14 +1,14 @@
-let lists = document.getElementById("lists");
+let pullDownButton= document.getElementById("lists");
     pullDownButton.addEventListener('click',function(){
     console.log("クリック");
 });
 
-let lists2 = document.getElementById("lists");
+let lists1 = document.getElementById("lists");
     pullDownButton.addEventListener('mouseover',function(){
     console.log("乗る");
 });
 
-let lists3 = document.getElementById("lists");
+let lists2 = document.getElementById("lists");
     pullDownButton.addEventListener('mouseout',function(){
-    console.log("外れる")
+    console.log("外れる");
 });
