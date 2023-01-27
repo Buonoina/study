8c4bf84adc66ae1fb4d8bbee111772d399b2c2ea
Buonoina <linghuadayo@gmail.com>
2023-01-27 11:53:37 +0900

    条件分岐編

--- java/study/stujava/index.js
+++ java/study/stujava/index.js
@@ -1,16 +1,27 @@
-let pullDownButton= document.getElementById("lists");
+const pullDownButton = document.getElementById("lists");
+const pullDownParents = document.getElementById("pull-down");
 
-    pullDownButton.addEventListener('click',function(){
-        pullDownButton.setAttribute("style","background-color:green;")
-    console.log("クリック");
+pullDownButton.addEventListener('click',function(){
+		this.setAttribute("style","background-color:green;")
+console.log("クリック");
 });
 
-    pullDownButton.addEventListener('mouseover',function(){
-        pullDownButton.setAttribute("style","background-color:blue;")
-    console.log("乗る");
+pullDownButton.addEventListener('mouseover',function(){
+		this.setAttribute("style","background-color:blue;")
+console.log("乗る");
 });
 
-    pullDownButton.addEventListener('mouseout',function(){
-        pullDownButton.removeAttribute("style","backgroung-color:red;")
-    console.log("外れる");
+pullDownButton.addEventListener('mouseout',function(){
+		this.removeAttribute("style","backgroung-color:red;")
+console.log("外れる");
+});
+
+pullDownButton.addEventListener('click',function(){
+if (pullDownParents.getAttribute("style") == "display:block;"){
+	pullDownParents.removeAttribute("style","display:block;")
+		console.log("表示");
+	}else {
+		pullDownParents.setAttribute("style","display:block;")
+		console.log("非表示");
+	}
 });
